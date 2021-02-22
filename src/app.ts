52776360fc4f6815292af530e5237cc84b7dea4c
mateugazi <mateugazi182@outlook.com>
2021-02-22 13:20:40 +0100

    Added connection to the mongodb

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
+require('dotenv').config()
 import express, { Request, Response, NextFunction, Router, Express } from 'express';
+import mongoose from 'mongoose'
 
 const app = express();
+
+mongoose.connect(`mongodb+srv://${process.env.MONGO_ATLAS_USER}:${process.env.MONGO_ATLAS_PASSWORD}@eduplatform.woboc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => console.log("mongodb connected")).catch(err => console.log(err))
+
 
 export default app;

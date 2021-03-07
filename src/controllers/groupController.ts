2c1c50c1d955178b49339d8daf90f0f7ded5e7ef
mateugazi <mateugazi182@outlook.com>
2021-03-07 19:59:49 +0100

    Added error handling to groupCreateGroup

--- src/controllers/groupController.ts
+++ src/controllers/groupController.ts
@@ -5,31 +5,37 @@ import userSchema from '../models/userSchema';
 
 export const groupCreateGroup = async (req: Request, res: Response) => {
   const userMentor = await userSchema.findById(req.body.mentor)
-  if (!userMentor) {
-    return res.status(404).json({
-      message: "Mentor not found"
-    })
-  }
-  else {
-    const groupName = await groupSchema.findOne({ groupName: req.body.groupName })
-    if (groupName) {
-      res.status(404).json({
-        message: "This group name is already taken"
+    try {
+    if (!userMentor) {
+      return res.status(404).json({
+        message: "Mentor not found"
       })
     }
     else {
-      const group = new groupSchema({
-        _id: mongoose.Types.ObjectId(),
-        groupName: req.body.groupName,
-        mentor: userMentor
-      })
-      console.log(group)
-      res.status(201).json({
-        message: "Group created",
-        createdGroup: {
-          group
-        }
-      })
+      const groupName = await groupSchema.findOne({ groupName: req.body.groupName })
+      if (groupName) {
+        res.status(404).json({
+          message: "This group name is already taken"
+        })
+      }
+      else {
+        const group = new groupSchema({
+          _id: mongoose.Types.ObjectId(),
+          groupName: req.body.groupName,
+          mentor: userMentor
+        })
+        console.log(group)
+        res.status(201).json({
+          message: "Group created",
+          createdGroup: {
+            group
+          }
+        })
+      }
     }
+  } catch (err) {
+    res.status(500).json({
+      err
+    })
   }
 };

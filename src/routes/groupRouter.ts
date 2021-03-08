d055b0c8c4bae10127a585acd4e9337d60731f9a
mateugazi <mateugazi182@outlook.com>
2021-03-08 17:32:09 +0100

    Added groupGetAllGroup to the groupRouter.ts

--- src/routes/groupRouter.ts
+++ src/routes/groupRouter.ts
@@ -1,9 +1,10 @@
 import express from 'express';
-import { groupCreateGroup, groupGetSingleGroup } from '../controllers/groupController';
+import { groupCreateGroup, groupGetSingleGroup, groupGetAllGroup } from '../controllers/groupController';
 
 const groupRouter = express.Router();
 
 groupRouter.post('/createGroup', groupCreateGroup);
+groupRouter.get('/', groupGetAllGroup)
 groupRouter.get('/:groupId', groupGetSingleGroup)
 
 export default groupRouter;

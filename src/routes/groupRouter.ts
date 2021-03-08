30672209e52390c1b2623ccc08f2d2f8319f886a
mateugazi <mateugazi182@outlook.com>
2021-03-08 17:24:24 +0100

    Added groupGetSingleGroup to the groupRouter.ts

--- src/routes/groupRouter.ts
+++ src/routes/groupRouter.ts
@@ -1,8 +1,9 @@
 import express from 'express';
-import { groupCreateGroup } from '../controllers/groupController';
+import { groupCreateGroup, groupGetSingleGroup } from '../controllers/groupController';
 
 const groupRouter = express.Router();
 
 groupRouter.post('/createGroup', groupCreateGroup);
+groupRouter.get('/:groupId', groupGetSingleGroup)
 
 export default groupRouter;

dbfdeded437388f98bb6693267d7f888a996b12b
Kinga Mamak <kingam280@gmail.com>
2021-03-11 20:49:22 +0100

    Add tests for other endpoints

--- spec/tests/projectsTest.spec.ts
+++ spec/tests/projectsTest.spec.ts
@@ -1,39 +1,30 @@
 import mongoose from 'mongoose';
 import Project from '../../src/models/projectSchema';
-import projectRouter from '../../src/routes/projectsRouter'
+import projectsRouter from '../../src/routes/projectsRouter'
 import express from 'express';
 import bodyParser from 'body-parser';
-const supertest = require('supertest');
-
-const app = express()
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json())
-app.use("/", projectRouter);
+import supertest from 'supertest'
+import app from '../../src/app'
 
 const request = supertest(app)
 
 const newProject = {
     _id: new mongoose.Types.ObjectId(),
-    title: "First project",
+    title: "test project",
     description: "This is first project",
-    mentor: "Józef",
-    authors: ["Ania", "Kasia"],
-    linkToDemo: 'www.demo.pl',
-    linkToGitHub: 'www.github.com/project',
+    mentor: "test",
+    authors: ["test", "test"],
+    linkToDemo: 'www.test.pl',
+    linkToGitHub: 'www.test.com/test',
     timestamp: Date.now()
 }
 
-const databaseName = 'projectsTest';
+const databaseName = 'rest-mongoose-test';
 
-describe('projects', () => {
-    beforeAll(async () => {
-        const url = `mongodb://127.0.0.1/${databaseName}`
-        await mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
-    })
+describe('/projects', () => {
 
     afterEach(async () => {
-        await Project.deleteMany()
+        await Project.deleteMany({title: "test project"})
     })
 
     it('create project successfully', () => {
@@ -47,12 +38,36 @@ describe('projects', () => {
         expect(project.linkToGitHub).toBe(newProject.linkToGitHub);
     });
 
-    it ('GET /', async done => {
-        const response = await request.get('/')
+    it('GET /projects', async (done) => {
+        const response = await request.get('/projects')
         expect(response.status).toBe(200)
-        
-        // await request.get('/')
-        // .expect(200)
+
+        done()
+    })
+
+    it('GET /projects', async (done) => {
+        const project = new Project(newProject)
+        await project.save()
+        const response = await request.get('/projects/' + project._id)
+        expect(response.status).toBe(200)
+
+        done()
+    })
+
+    it('POST /projects', async (done) => {
+        const response = await request.post('/projects')
+            .send(newProject)
+            .set('Accept', 'application/json')
+        expect(response.status).toBe(201)
+
+        done()
+    })
+
+    it('DELETE /projects/:id', async (done) => {
+        const project = new Project(newProject)
+        await project.save()
+        const response = await request.delete('/projects/' + project._id)
+        expect(response.status).toBe(200)
 
         done()
     })

b4f607ada8815499b89a264dd9427f1bfd48cee1
neacatsu <wiktoria.wierzbicka166@gmail.com>
2021-03-15 21:10:48 +0100

    added tests

--- src/controllers/authorizationController.ts
+++ src/controllers/authorizationController.ts
@@ -1,47 +1,57 @@
 import {Request, Response} from "express";
-import {User, IUser} from "../models/userSchema";
+import {User, IUser} from "../models/authorizationSchema";
 import mongoose from "mongoose";
-import {MongoError, Collection} from "MongoDB"
-import bcrypt from "bcrypt"
-import jwt from "jsonwebtoken"
+import {MongoError, Collection} from "MongoDB";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
-const checkLoginForm = (object: any): Object => {
+
+
+export const checkLoginForm = (object: any): Object => {
+    if(!object.login) return { error: "There is not login passed" };
     const reg = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    if(reg.test(object.login)) return { email: object.login }
-    return { login: object.login }
+    if(reg.test(object.login)) return { email: object.login };
+    return { login: object.login };
 }
 
-const isValidPassword = (db_password: any, password: any): boolean => {
-    return bcrypt.compareSync(password, db_password)
+export const isValidPassword = (db_password: any, password: any): boolean => {
+    return bcrypt.compareSync(password, db_password);
 }
 
 // Sprawdzanie dostepnych uzytkownikow w bazie
-export const ShowAll = (request: Request, response:Response): void => {
-    mongoose.connection.db.collection("userschemas", function(err: MongoError, collection: Collection){
+export const ShowAll = (request: Request, response: Response): void => {
+    mongoose.connection.db.collection("users", function(err: MongoError, collection: Collection){
+        if(err) {
+            response.status(400).send(err);
+        } else {
         collection.find({}).toArray(function(err: MongoError, data: any[]){
-            response.status(200).send(data)
+            response.status(200).send(data);
         })
+        }
     });
 }
 
-export const LogIn = (request: Request, response:Response) => { 
-    mongoose.connection.db.collection("userschemas", function (err: MongoError, collection: Collection) {       
-        collection.findOne(checkLoginForm(request.query), function (err: MongoError, data: IUser) {    
-
-            if(!data || !isValidPassword(data.password, request.query.password)) { 
-                return response.status(401).json({ message: 'Authentication failed. Invalid user or password.' });
-            } else {
-                return response.json({ token: jwt.sign({ 
-                    _id: data._id,
-                    firstName: data.firstName,
-                    lastName: data.lastName,
-                    email: data.email,
-                    password: data.password,
-                    login: data.login,
-                    role: data.role
-                }, 'Authentication', { expiresIn: '1h' }) })
-            }
-        })
+export const LogIn = (request: Request, response: Response) => { 
+    mongoose.connection.db.collection("users", function (err: MongoError, collection: Collection) {
+        if(err) {
+            response.status(400).send(err);
+        } else {
+            collection.findOne(checkLoginForm(request.query), function (err: MongoError, data: IUser) {    
+                if(!data || !isValidPassword(data.password, request.query.password)) { 
+                    return response.status(401).json({ message: 'Authentication failed. Invalid user or password.' });
+                } else {
+                    return response.json({ token: jwt.sign({ 
+                        _id: data._id,
+                        firstName: data.firstName,
+                        lastName: data.lastName,
+                        email: data.email,
+                        password: data.password,
+                        login: data.login,
+                        role: data.role
+                    }, 'Authentication', { expiresIn: '1h' }) });
+                }
+            })
+        }
     })
 }
 
@@ -51,23 +61,16 @@ export const Register = (request: Request, response: Response): void => {
         firstName: request.query.firstName,
         lastName: request.query.lastName,
         email: request.query.email,
-        login: request.query.login,
         password: bcrypt.hashSync(request.query.password, 10),
+        login: request.query.login,
         role: request.query.role
-    })
+    });
 
     user.save()
-<<<<<<< HEAD
         .then((item:any) => {
             response.send("Item saved to DB");
         })
         .catch((err:any) => {
-=======
-        .then(item => {
-            response.send("Item saved to DB");
-        })
-        .catch(err => {
->>>>>>> 945ca1c (Rebasing 1/2)
             response.status(400).send(`Unable to save item ${err}`);
         });
 }
--- src/models/userSchema.ts
+++ src/models/userSchema.ts
@@ -10,7 +10,7 @@ export interface IUser extends Document{
     role:string
 }
 
-const userSchema = new mongoose.Schema({
+export const userSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     firstName: {
         type: String,

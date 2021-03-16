16a8899c099d7ae171b016b25b167be59a2bdd96
JotGabiga <jot.gabiga@gmail.com>
2021-03-16 13:10:20 +0100

    Swagger description added

--- swagger.js
+++ swagger.js
@@ -11,12 +11,11 @@ const doc = {
     },
     host: "eduplatformapi.herokuapp.com",
     basePath: "/",
-    
+
     schemes: ['https'],
     consumes: ['application/json'],
     produces: ['application/json'],
-    tags: [
-        {
+    tags: [{
             name: "Groups",
             description: "Endpoints"
         },
@@ -54,46 +53,67 @@ const doc = {
         }
     },
     definitions: {
-            group: {
+        group: {
+            _id: "604a7d278316be1be42ecab0",
+            groupName: "sample group name",
+            mentor: "604a7b12d610101287aa2955",
+            members: [{
+                _id: "604a7ba6d610101287aa2957"
+            }]
+        },
+        allGroups: {
+            numberOfGroups: 1,
+            result: [{
                 _id: "604a7d278316be1be42ecab0",
                 groupName: "sample group name",
                 mentor: "604a7b12d610101287aa2955",
-                members: [
-                    {
-                        _id: "604a7ba6d610101287aa2957"
-                    }
-                ]
+                members: [{
+                    _id: "604a7ba6d610101287aa2957"
+                }]
+            }]
+        },
+
+        createGroup: {
+            $mentor: "604a7b12d610101287aa2955",
+            $groupName: "sample group name"
+        },
+        addMember: {
+            $_id: "604a7ba6d610101287aa2957"
+        },
+        deleteMember: {
+            $_id: "604a7ba6d610101287aa2957"
+        },
+        changeName: {
+            $newName: "new group name"
+        },
+        allAnnouncements: [{
+                _id: "604fd0d798161c531cc51c03",
+                title: "Title1",
+                type: "type",
+                content: "Lorem Ipsum..."
             },
-            allGroups: {
-                numberOfGroups: 1,
-                result: [
-                    {
-                        _id: "604a7d278316be1be42ecab0",
-                    groupName: "sample group name",
-                    mentor: "604a7b12d610101287aa2955",
-                    members: [
-                        {
-                            _id: "604a7ba6d610101287aa2957"
-                        }
-                    ]
-                }
-                ]
-            },
-            
-            createGroup: {
-                $mentor: "604a7b12d610101287aa2955",
-                $groupName: "sample group name"
-            },
-            addMember: {
-                $_id: "604a7ba6d610101287aa2957"
-            },
-            deleteMember: {
-                $_id: "604a7ba6d610101287aa2957"
-            },
-            changeName: {
-                $newName: "new group name"
+            {
+                _id: "604fd0eb98161c531cc51c04",
+                title: "Title2",
+                type: "type",
+                content: "Becon Ipsummmm..."
             }
-        }
+        ],
+        announcementWithId: {
+            _id: "604fd0eb98161c531cc51c04",
+            title: "sample announcement name",
+            content: "sample announcement content",
+            type: "sample type"
+        },
+        announcement: {
+            title: "sample announcement name",
+            content: "sample announcement content",
+            type: "sample type"
+        },
+        updateAnnouncement: {
+            title: "new announcement name",
+        },
+    }
 }
 
 swaggerAutogen(outputFile, endpointsFiles, doc)
